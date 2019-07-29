d69627fafec5dcad60274926184d73af6634db8a
Orion Free <orionstein@gmail.com>
2019-07-29 01:39:26 -0400

    Adding initial framework

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Helmet from 'react-helmet'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Helmet title="Karina and Orion Wedding" />
+      <header className="App-header">
+        <p>
+          Karina and Orion WEDDING AAAH
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
