234cc90a4456801c4b763848b33af1551ab1ab70
Orion Free <orionstein@gmail.com>
2019-07-30 13:39:25 -0400

    Preact

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -271,6 +271,12 @@ module.exports = function(webpackEnv) {
       alias: {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
+        'react': 'preact-compat',
+        'react-dom': 'preact-compat',
+        // Not necessary unless you consume a module using `createClass`
+        'create-react-class': 'preact-compat/lib/create-react-class',
+        // Not necessary unless you consume a module requiring `react-dom-factories`
+        'react-dom-factories': 'preact-compat/lib/react-dom-factories',
         'react-native': 'react-native-web',
       },
       plugins: [
